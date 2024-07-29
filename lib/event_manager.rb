df68cee5a844ff22dfa77860973c665cc1a4e41d
ZephyRemix <hunyaochong@gmail.com>
2024-07-29 19:47:24 +0800

    Add function to automate thank you messages

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,9 @@
-require 'google/apis/civicinfo_v2'
 require 'csv'
-require 'colorize'
+require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')[0..4]
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zip)
@@ -11,32 +11,44 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name).join(", ")
-  rescue 
+    ).officials
+  rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-puts 'Event Manager Initialized!'
+puts 'EventManager initialized.'
 
 contents = CSV.open(
-  'event_attendees.csv', 
+  'event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
-  
-  puts "#{name} #{zipcode} #{legislators}"
+
+  form_letter = erb_template.result(binding)
+
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+
 end
+
